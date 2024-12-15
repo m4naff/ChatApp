e31ac8712320e0f1fd38534fa1a320d1175a9ed8
themanaff <ahmedovmenaf@gmail.com>
2024-12-16 01:18:59 +0400

    BaseDocument added

--- src/main/java/com/example/chatapp/model/User.java
+++ src/main/java/com/example/chatapp/model/User.java
@@ -1,4 +1,13 @@
 package com.example.chatapp.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * User object model (MongoDB document).
+ */
+
 public class User {
+
 }
