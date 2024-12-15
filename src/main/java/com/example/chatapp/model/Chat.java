169500ee68f84a91bd323bc5e11e11824c10711d
themanaff <ahmedovmenaf@gmail.com>
2024-12-16 01:36:04 +0400

    Chat class is ready

--- src/main/java/com/example/chatapp/model/Chat.java
+++ src/main/java/com/example/chatapp/model/Chat.java
@@ -1,4 +1,27 @@
 package com.example.chatapp.model;
 
-public class Chat {
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+
+/**
+ * Chat object model (MongoDB Document).
+ */
+@Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = false)
+@Document(collection = "chats")
+public class Chat extends BaseDocument {
+
+    private String name;
+
+    @NotNull
+    @DBRef // TODO replace with manual references so non-reactive MongoDB dependency can be removed
+    private Set<User> members;
+
 }
