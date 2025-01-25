f9b12fde8b3ba480774bd7fd6e0bfbeb14db1401
themanaff <ahmedovmenaf@gmail.com>
2025-01-25 23:07:17 +0400

    deleteChat method added

--- src/main/java/com/example/chatapp/servcie/ChatService.java
+++ src/main/java/com/example/chatapp/servcie/ChatService.java
@@ -5,12 +5,17 @@ import com.example.chatapp.model.User;
 import com.example.chatapp.repository.ChatRepository;
 import com.example.chatapp.utils.SecurityUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.ChangeStreamEvent;
+import org.springframework.data.mongodb.core.ChangeStreamOptions;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 import java.util.HashSet;
 import java.util.Objects;
@@ -47,6 +52,20 @@ public class ChatService {
         return chatRepository.deleteById(chatId);
     }
 
+    public Flux<Chat> streamNewChats() {
+        ChangeStreamOptions options = ChangeStreamOptions.builder()
+                .filter(Aggregation.newAggregation(Aggregation.match(Criteria.where("operationType").is("insert"))))
+                .build();
+
+        return mongoOperations
+                .changeStream("chats", options, Chat.class)
+                .mapNotNull(ChangeStreamEvent::getBody)
+                .zipWith(SecurityUtils.getPrincipal())
+                .filter(tuple -> tuple.getT1().getMembers().contains(tuple.getT2().user()))
+                .map(Tuple2::getT1);
+    }
+    
+    
 
 
 }
